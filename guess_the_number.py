36ed6e20ced8d0a02a344e19881ad88a5f936726
Ammad-Younas <79741234+Ammad-Younas@users.noreply.github.com>
2021-03-05 08:09:17 +0500

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,7 +1,7 @@
-#Author 'Ammad Younas'
 import random
 stored_number = random.randint(0, 1000)
 class color:
+	underline='\033[04m'
 	bold='\033[01m'
 	blue='\033[34m'
 	orange='\033[33m'
@@ -9,22 +9,29 @@ class color:
 	red='\033[31m'
 	green='\033[32m'
 	cyan='\033[36m'
+	purple='\033[35m'
 	darkgrey = '\033[90m'
+	lightcyan='\033[96m'
 attempt = 1
-print(color.darkgrey, "              Made By @Ammad !!!")
-print(color.bold, color.blue, "Welcome to guess the number Game  b/w  1 to 1000")
-print(color.orange, "            You have only 15 attempts")
-while (attempt<=15):
-	user_input = int(input(" Enter a Number: "))
-	if user_input < stored_number:
-		print(color.gold, "Enter Greater Number and attempt left:", 15-attempt)
-	elif user_input > stored_number:
-		print(color.red, "Enter Lesser Number and attempt left:", 15-attempt)
-	else:
-		print(color.green ,"Congratulation ! You Win the Game In", attempt, "Attempt")
-		break
-	attempt = attempt + 1
-if (attempt>15):
-	print(color.cyan, "         Game over")
-	print(color.cyan, "You Have Run Out Of Attempts")
-	print("  The Stored number Was", [stored_number])
+try:
+	print(color.darkgrey, "              Made By @Cybernetic !!!")
+	print(color.bold, color.blue, "Welcome to guess the number Game  b/w  1 to 1000")
+	print(color.orange, "            You have only 15 attempts")
+	while (attempt<=15):
+		print(color.purple," Enter a Number: ", end="")
+		user_input = int(input(""))
+		if user_input < stored_number:
+			print(color.gold, " Enter Greater Number and attempt left:", 15-attempt)
+		elif user_input > stored_number:
+			print(color.red, " Enter Lesser Number and attempt left:", 15-attempt)
+		else:
+			print(color.green ," Congratulation ! You Win the Game In", attempt, "Attempt")
+			break
+		attempt = attempt + 1
+	if (attempt>15):
+		print(color.cyan, "           Game over")
+		print(color.cyan, " You Have Run Out Of Attempts")
+		print("  The Stored number Was", [stored_number])
+except Exception as error:
+	print(color.lightcyan, " You Entered Wrong Input")
+	print(color.bold, color.lightcyan, "Please !!! Enter An Integer Value Not String....")
